b6592fee94b361c158f14cb23ef3f94bb1c9acf9
Kyle Thomas <kthomas852@yahoo.com>
2019-02-21 13:54:36 -0700

    added customer list

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import "./App.css";
 import NewItem from "./components/NewItem";
 import RentingForm from './components/RentingForm';
 import NewCustomer from './components/newCustomer';
+import CustomerList from './components/CustomerList';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>WRECK UR LIFE</h1>
-
-        <NewItem />
-        <RentingForm />
-        <NewCustomer />
+         <Nav />
+        <Switch>
+          <Route exact path="/" component={RentingForm} />
+          <Route exact path="/newItem" component={NewItem} />
+          <Route exact path="/newCustomer" component={NewCustomer} />
+          <Route exact path="/customerList" component={CustomerList} />
+          {/* <Route exact path="/history" component={History} /> */}
+        </Switch>
       </div>
     );
   }
