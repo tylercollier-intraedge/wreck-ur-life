bcfc35374123b8b1e7b83575066e8c609235fda5
jorlee92 <jorlee92@gmail.com>
2019-02-21 10:46:20 -0700

    fixed seed script

--- server/scripts/seedDB.js
+++ server/scripts/seedDB.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const dotenv = require('dotenv').config();
 
 mongoose.connect(
   process.env.MONGODB_URI ||
   "mongodb://localhost/stickyBoard"
-);
+, { useNewUrlParser: true });
 
 const noteSeed = [
   {
@@ -18,14 +19,52 @@ const noteSeed = [
   }
 ];
 
-db.Note
-  .remove({})
-  .then(() => db.Note.collection.insertMany(noteSeed))
+const sampleEquipment = [{ type: "Lawnmower", pictureURL: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/John_Deere_lawn_mower.JPG/1280px-John_Deere_lawn_mower.JPG"}, { title: "Kayak", pictureURL: "http://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Runaground.jpg/1280px-Runaground.jpg"}, {type: "Snow-Plow", pictureURL: "http://upload.wikimedia.org/wikipedia/commons/9/9f/Sidewalk_plow.JPG"}]
+
+const sampleUsers = [
+  { name: "Jordan", email: "test@com.com", phoneNumber: 18005554040},
+  { name: "John", email: "john@com.com", phoneNumber: 1861856165841},
+  { name: "Mark", email: "mark@com.com", phoneNumber: 1135861384634},
+  { name: "Sally", email: "sally@com.com", phoneNumber: 183513813661},
+]
+
+const createRecords = async () => {
+  let equipIds;
+  let userIds;
+  await db.User.deleteMany({})
+  .then(() => db.User.collection.insertMany(sampleUsers))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
+    userIds = data.insertedIds;
+    console.log(data.result.n + " user records inserted!");
+  })
+  await db.Equipment
+  .deleteMany({})
+  .then(() => db.Equipment.collection.insertMany(sampleEquipment))
+  .then(data => {
+    equipIds = data.insertedIds;
+    console.log(data.result.n + " equipment records inserted!");
   })
   .catch(err => {
     console.error(err);
     process.exit(1);
   });
+
+//Source: https://stackoverflow.com/questions/23081158/javascript-get-date-of-the-next-day
+  var today = new Date();
+  var tomorrow = new Date();
+  var twoDaysFromNow = new Date();
+  tomorrow.setDate(today.getDate()+1);
+  twoDaysFromNow.setDate(today.getDate+2);
+  const sampleRentals = [
+    { user_id: userIds[0], user_fullname: "Jordan", equipment_id: equipIds[0], equipment_name: "Lawnmower", rental_date: tomorrow },     { user_id: userIds[1], user_fullname: "John", equipment_id: equipIds[2], equipment_name: "Snow-Plow", rental_date: today },
+    { user_id: userIds[2], user_fullname: "Mark", equipment_id: equipIds[2], equipment_name: "Snow-Plow", rental_date: twoDaysFromNow }
+  ]
+  await db.Rental
+  .deleteMany({})
+  .then(() => db.Rental.collection.insertMany(sampleRentals))
+  .then((data) => {
+    console.log(data.result.n + " rental records inserted!");
+    process.exit(1);
+  })
+}
+createRecords();
