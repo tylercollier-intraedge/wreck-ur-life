0ed0a518daedd909416d1825f1db264a7b467a5c
adamdrag <adam.drag@yahoo.com>
2019-02-21 09:44:19 -0700

    rentingForm react

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default {
   getAllUsers: () => axios.get('/api/users'),
-  getAllEquipments: () => axios.get('api/equipments')
+  getAllEquipments: () => axios.get('api/equipments'),
+  addNewRenting: rentingData => axios.post('/api/rentals', rentingData),
 };
--- server/models/equipment.js
+++ server/models/equipment.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const equipmentSchema = new Schema({
-  type: String,
-  title: String,
+  name: String,
   pictureURL: String,
 });
 
