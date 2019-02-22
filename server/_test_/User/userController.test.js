cf1b9aab29c21968f6983981a51f02047aa41823
adamdrag <adam.drag@yahoo.com>
2019-02-21 18:52:08 -0700

    testing

---
+++ server/_test_/User/userController.test.js
@@ -0,0 +1,20 @@
+import { isFunction } from 'lodash';
+import controllers from '../../controllers/usersController';
+
+describe('item controllers', () => {
+  test('has crud controllers', () => {
+    const crudMethods = [
+      'findAll',
+      'findById',
+      'create',
+      'update',
+      'remove'
+    ]
+
+    crudMethods.forEach(name =>
+      expect(isFunction(controllers[name])).toBe(true)
+    )
+  })
+
+  
+})
---
+++ server/_test_/User/userModel.test.js
@@ -0,0 +1,39 @@
+// const { expect } = require('chai')
+const usersModel = require('../../models/user')
+// const mongoose = require('mongoose');
+
+describe('user Model', () => {
+    it('check users collection schema types', () => {
+        const user = usersModel.schema.obj.name;
+        expect(user).toEqual({
+            type: String,
+            required: true
+        });
+    })
+    it('check date collection schema types', () => {
+        const user = usersModel.schema.obj.email;
+        expect(user).toEqual({
+            type: String,
+            required: true
+        })
+    })
+    it('check date collection schema types', () => {
+        const user = usersModel.schema.obj.phoneNumber;
+        expect(user).toEqual({
+            type: Number,
+            required: true
+        })
+    })
+    it('check date collection schema types', () => {
+        const user = usersModel.schema.obj.rentalHistory;
+        expect(user).toEqual({
+            type: Array
+        })
+    })
+    it('check date collection schema types', () => {
+        const user = usersModel.schema.obj.currentRentals;
+        expect(user).toEqual({
+            type: Array
+        })
+    })
+})
